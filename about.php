16710a61e0547a8276cf0da13650a2bfa5b424b9
Bastian <bastian@ch3rr1.me>
2014-06-07 14:08:41 +0200

    Looking for structure.

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php require('header.php'); ?>
+<main>
+	<?php require('sidebar-left.php'); ?>
+	<section id="content">
+		Hier stehen eine Beschreibung.
+	</section>
+	<?php require('sidebar-right.php'); ?>
+</main>
+<?php require('footer.php'); ?>
