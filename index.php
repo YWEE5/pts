e022073f8c188759b5b538c83a8a864608cdad73
Bastian <bastian@ch3rr1.me>
2014-06-07 14:47:19 +0200

    Just a comment.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <main>
 	<?php require('sidebar-left.php'); ?>
 	<section id="content">
+		<!-- needs a fine loop to look for the right content -->
 		Dynamischer Content
 	</section>
 	<?php require('sidebar-right.php'); ?>
