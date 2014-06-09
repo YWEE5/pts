dd82d3cefc5801ac77fd1005933f823082485faa
Bastian <bastian@ch3rr1.me>
2014-06-09 18:14:12 +0200

    Specified exact row.

--- class.php
+++ class.php
@@ -11,7 +11,7 @@ try{
 		echo 'Keine Datensätze gefunden!';
 	} else {
 		foreach($result as $row) {
-			echo $row['Vorname'] . ' ' . $row['Nachname'] . '<br />';
+			echo $row['Bezeichnung'] . '<br />';
 		}
 	}
 }
