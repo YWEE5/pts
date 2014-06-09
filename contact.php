08d9654916aa4021cfe2777da8d78af5577295ff
Bastian <bastian@ch3rr1.me>
2014-06-09 18:28:08 +0200

    Added content.

---
+++ contact.php
@@ -0,0 +1,9 @@
+<form>
+	Name<br />
+	<input type="text" name="name"><br />
+	Mail<br />
+	<input type="text" name="mail"><br />
+	Nachricht<br />
+	<input type="text" name="message"><br />
+	<input type="submit" value="Absenden">
+</form>
